7ede84f19c77b397bb6f45566822919453cd566a
chuu100 <106085848+chuu100@users.noreply.github.com>
2022-12-07 21:58:45 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,1 +1,16 @@
-import org.junit.jupiter.api.Disabled;import org.junit.jupiter.api.Test;import org.junit.jupiter.api.Timeout;import java.util.concurrent.TimeUnit;import static org.junit.jupiter.api.Assertions.*;class MainTest {    @Disabled    @Test    @Timeout(value = 22)    void main() throws Exception {        Main.main(null);    }}
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Disabled
+    @Test
+    @Timeout(value = 22)
+    void main() throws Exception {
+        Main.main(null);
+    }
+}
